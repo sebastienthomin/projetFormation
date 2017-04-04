ab79b59b6865aeb495e5a5e921ceaccc5cd02888
sebastienthomin <sebastien.thomin@gmail.com>
2017-04-04 12:17:54 +0200

    test message

---
+++ src/projetFormation/Test.java
@@ -0,0 +1,12 @@
+package projetFormation;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("MAILIS LA PLUS BELLE");
+
+	}
+
+}
